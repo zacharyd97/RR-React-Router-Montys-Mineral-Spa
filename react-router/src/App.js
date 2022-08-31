2953c2bd924e54c3a425a7047a4b54378da1bfa7
Zachary Dowell <zdowell97@gmail.com>
2022-08-31 01:15:18 -0400

    css bootstrAP

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
-
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import './App.css';
-
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
@@ -20,22 +20,26 @@ function App() {
       <Router>
       <header>
         <h1 className="title">Welcome to Monty's Mineral SPA</h1>
-
-        <div className="navBar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/packages">Our Packages</Link>
-            </li>
-          </ul>
-        </div>
+        <Container>
+          <Nav defaultActiveKey="/" variant="tabs" fill>
+              <Nav.Item>
+                  <Nav.Link href="/"> 
+                      <Link to="/">Home</Link>
+                  </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                  <Nav.Link eventKey={"aboutPage"}> 
+                      <Link to="/about">About Us</Link>
+                  </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                  <Nav.Link eventKey={"packagesPage"}> 
+                      <Link to="/packages">Our Packages</Link> 
+                  </Nav.Link>
+              </Nav.Item>
+          </Nav>
+        </Container>
       </header>
-      
       <div className='display'>
         <Routes>
           <Route path="/" element={<Home/>}/>
@@ -50,8 +54,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,19 +1,25 @@
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage) => 
+        <ListGroup.Item>{eachPackage}</ListGroup.Item>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-                
-            </div>
-        </div>
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Our Packages</Card.Title>
+                    <Card.Text>
+                        Check out some of our packages! Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
+                    {displayPackages} 
+                </ListGroup>
+            </Card>
+        </Container>
     )
 }
